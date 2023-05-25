5831636e8e750e97f0e925d018dab2a0c83e0a9b
jasper.zuallaert <jasper.zuallaert@gmail.com>
2023-05-25 18:05:11 +0200

    Initial commit

---
+++ example.py
@@ -0,0 +1,39 @@
+import language_models as lm
+
+example_sequences = [
+    'MKTVRQERLKSIVRILERSKEPVSGAQLAEELSVSRQVIVQDIAYLRSLGYNIVATPRGYVLAGG',
+    'KALTARQQEVFDLIRDHISQTGMPPTRAEIAQRLGFRSPNAAEEHLKALARKGVIEIVSGASRGIRLLQEE'
+]
+
+device = 'cuda'
+
+# print(f'Running ESM-1b...')
+# embedding, mask = lm.run_esm(example_sequences, 'ESM-1b', device=device)
+# print(f'Embedding shape: {embedding.shape}')
+# print(f'Mask shape: {mask.shape}')
+
+print(f'Running ESM-2-3B...')
+embedding, mask = lm.run_esm(example_sequences, 'ESM-2-3B', device=device)
+print(f'Embedding shape: {embedding.shape}')
+print(f'Mask shape: {mask.shape}')
+
+print(f'Running ESM-2-650M...')
+embedding, mask = lm.run_esm(example_sequences, 'ESM-2-650M', device=device)
+print(f'Embedding shape: {embedding.shape}')
+print(f'Mask shape: {mask.shape}')
+
+print(f'Running ProtTransT5_XL_UniRef50...')
+embedding, mask = lm.run_prottransxl(example_sequences, device=device)
+print(f'Embedding shape: {embedding.shape}')
+print(f'Mask shape: {mask.shape}')
+
+print(f'Running Ankh_base...')
+embedding, mask = lm.run_ankh(example_sequences, 'base', device=device)
+print(f'Embedding shape: {embedding.shape}')
+print(f'Mask shape: {mask.shape}')
+
+print(f'Running Ankh_large...')
+embedding, mask = lm.run_ankh(example_sequences, 'large', device=device)
+print(f'Embedding shape: {embedding.shape}')
+print(f'Mask shape: {mask.shape}')
+
